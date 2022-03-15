0c0a161e95ac46ed58c6204e68b3d4173361dbd0
KevinJonathan117 <kevinjonathan3010@gmail.com>
2022-03-15 22:09:12 +0700

    Added chart in analytics page

--- FeelJournal/Controller/AnalyticsViewController.swift
+++ FeelJournal/Controller/AnalyticsViewController.swift
@@ -5,9 +5,13 @@
 //  Created by Kevin Jonathan on 15/03/22.
 //
 
+import Charts
 import UIKit
 
-class AnalyticsViewController: UIViewController {
+class AnalyticsViewController: UIViewController, ChartViewDelegate {
+    
+    @IBOutlet var pieChartView: UIView!
+    var pieChart = PieChartView()
 
     @IBOutlet var filterControl: UISegmentedControl!
     @IBOutlet var feelingText: UILabel!
@@ -15,11 +19,65 @@ class AnalyticsViewController: UIViewController {
         super.viewDidLoad()
         feelingText.text = feelAverage7days
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        pieChart.delegate = self
+        generateChart(requestedData: "7days")
+    }
+    
+    func generateChart(requestedData: String) {
+        pieChart.frame = CGRect(x: 0, y: 0, width: pieChartView.frame.width, height: pieChartView.frame.width)
+        
+        var entries = [ChartDataEntry]()
+        
+        var happy: Int = 0, neutral: Int = 0, sad: Int = 0
+        
+        for journal in journalData {
+            if requestedData == "7days" {
+                if (Date() - journal.createdAt!).day! <= 7 {
+                    if journal.feeling == "Neutral" {
+                        neutral += 1
+                    } else if journal.feeling == "Sad" {
+                        sad += 1
+                    } else {
+                        happy += 1
+                    }
+                }
+            } else {
+                if journal.feeling == "Neutral" {
+                    neutral += 1
+                } else if journal.feeling == "Sad" {
+                    sad += 1
+                } else {
+                    happy += 1
+                }
+            }
+        }
+        if happy > 0 {
+            entries.append(ChartDataEntry(x: Double(happy), y: Double(happy)))
+        }
+        if neutral > 0 {
+            entries.append(ChartDataEntry(x: Double(neutral), y: Double(neutral)))
+        }
+        if sad > 0 {
+            entries.append(ChartDataEntry(x: Double(sad), y: Double(sad)))
+        }
+        
+        let set = PieChartDataSet(entries: entries)
+        set.colors = ChartColorTemplates.colorful()
+        let data = PieChartData(dataSet: set)
+        pieChart.data = data
+        pieChartView.addSubview(pieChart)
+    }
+    
     @IBAction func onFilterChange(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             feelingText.text = feelAverage7days
+            generateChart(requestedData: "7days")
         } else {
             feelingText.text = feelAverage
+            generateChart(requestedData: "alltime")
         }
     }
     
---
+++ Pods/Target Support Files/Pods-FeelJournal/Pods-FeelJournal-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FeelJournal : NSObject
+@end
+@implementation PodsDummy_Pods_FeelJournal
+@end
