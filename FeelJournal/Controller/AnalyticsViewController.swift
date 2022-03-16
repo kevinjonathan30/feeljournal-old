6c3b2a5b73bbddb41ebcc95d07c94eae32a70d8b
KevinJonathan117 <kevinjonathan3010@gmail.com>
2022-03-16 13:38:54 +0700

    Fixed analytics chart to show well in dark mode, changed colors and added labels

--- FeelJournal/Controller/AnalyticsViewController.swift
+++ FeelJournal/Controller/AnalyticsViewController.swift
@@ -14,9 +14,18 @@ class AnalyticsViewController: UIViewController, ChartViewDelegate {
 
     @IBOutlet var filterControl: UISegmentedControl!
     @IBOutlet var feelingText: UILabel!
+    
+    var happyDataEntry = PieChartDataEntry(value: 0)
+    var neutralDataEntry = PieChartDataEntry(value: 0)
+    var sadDataEntry = PieChartDataEntry(value: 0)
+    
+    var numberOfFeelingsDataEntries = [PieChartDataEntry]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         feelingText.text = feelAverage7days
+        pieChartView.chartDescription.text = ""
+        pieChartView.drawHoleEnabled = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,10 +35,6 @@ class AnalyticsViewController: UIViewController, ChartViewDelegate {
     }
     
     func generateChart(requestedData: String) {
-//        pieChart.frame = CGRect(x: 0, y: 0, width: pieChartView.frame.width, height: pieChartView.frame.width)
-        
-        var entries = [ChartDataEntry]()
-        
         var happy: Int = 0, neutral: Int = 0, sad: Int = 0
         
         for journal in journalData {
@@ -53,20 +58,30 @@ class AnalyticsViewController: UIViewController, ChartViewDelegate {
                 }
             }
         }
+        numberOfFeelingsDataEntries = []
         if happy > 0 {
-            entries.append(ChartDataEntry(x: Double(happy), y: Double(happy)))
+            happyDataEntry.value = Double(happy)
+            happyDataEntry.label = "Happy"
+            numberOfFeelingsDataEntries.append(happyDataEntry)
         }
         if neutral > 0 {
-            entries.append(ChartDataEntry(x: Double(neutral), y: Double(neutral)))
+            neutralDataEntry.value = Double(neutral)
+            neutralDataEntry.label = "Neutral"
+            numberOfFeelingsDataEntries.append(neutralDataEntry)
         }
         if sad > 0 {
-            entries.append(ChartDataEntry(x: Double(sad), y: Double(sad)))
+            sadDataEntry.value = Double(sad)
+            sadDataEntry.label = "Sad"
+            numberOfFeelingsDataEntries.append(sadDataEntry)
         }
         
-        let set = PieChartDataSet(entries: entries)
-        set.colors = ChartColorTemplates.colorful()
-        let data = PieChartData(dataSet: set)
-        pieChartView.data = data
+        let chartDataSet = PieChartDataSet(entries: numberOfFeelingsDataEntries, label: "")
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let colors = [UIColor(red: 88/255.0, green: 86/255.0, blue: 214/255.0, alpha: 1.0), UIColor(red: 126/255.0, green: 33/255.0, blue: 212/255.0, alpha: 1.0), UIColor(red: 150/255.0, green: 131/255.0, blue: 236/255.0, alpha: 1.0)]
+        chartDataSet.colors = colors
+        
+        pieChartView.data = chartData
     }
     
     @IBAction func onFilterChange(_ sender: UISegmentedControl) {
@@ -78,5 +93,32 @@ class AnalyticsViewController: UIViewController, ChartViewDelegate {
             generateChart(requestedData: "alltime")
         }
     }
-    
+}
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
 }
